4fb587138bbb6d22b8d718c0e8a9773b19650b79
Cewtwo <128930660+Cewtwo@users.noreply.github.com>
2023-11-12 13:46:40 +0100

    Add files via upload

---
+++ aktuelles.js
@@ -0,0 +1,20 @@
+var template = {
+    "layout": {
+      "header": {
+        "image": {
+          "src": "grosserbelt-Lage.jpg",
+          "alt": "Das Bild konnte nicht geladen werden.",
+          "position": "links oben"
+        },
+        "heading": {
+          "text": "Aktueller Test",
+          "size": "2em"
+        },
+        "paragraph": {
+          "text": "Dies ist ein Test-TExt, der zeigen soll, wie ein aktuelles aussehen könnte. Er soll Herr Kratzer, aber vor allem Manesh zufrieden stellen. Im anschluss lesen sie noch ein bisschen Geschwurbel, das einfach nur ein Paar Zeilen füllen soll: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+          "size": "1em"
+        }
+      }
+    }
+  };
+  
