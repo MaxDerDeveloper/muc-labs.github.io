9cb43f53d52cd0ed88a00dc24e1d0c5000ebfd4a
Max <maximilianweiser@outlook.de>
2023-12-09 12:11:34 +0100

    Run.bat verbessert

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
 
-x86_64-pc-windows-msvc-simple-http-server.exe --nocache --ip  192.168.2.107 --port 80
+:: Wartet 0.5 Sekunden und öffnet die Webseite auf dem lokalen Webserver
+start cmd.exe /c "timeout /t 0.5 & start http://localhost:8080/index.html"
+
+:: Startet den lokalen Webserver
+x86_64-pc-windows-msvc-simple-http-server.exe --nocache --ip  127.0.0.1 --port 8080
