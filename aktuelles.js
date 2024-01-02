b213d255c98384c14fcb452d4244df587115d712
Cewtwo <128930660+Cewtwo@users.noreply.github.com>
2024-01-02 15:38:09 +0100

    Rework von aktuelles

--- aktuelles.js
+++ aktuelles.js
@@ -1,20 +1,45 @@
-var template = {
-    "layout": {
-      "header": {
-        "image": {
-          "src": "/bilder/welpe1.jpg",
-          "alt": "Das Bild konnte nicht geladen werden.",
-          "position": "links oben"
-        },
-        "heading": {
-          "text": "Aktueller Test",
-          "size": "2em"
-        },
-        "paragraph": {
-          "text": "Dies ist ein Test-TExt, der zeigen soll, wie ein aktuelles aussehen könnte. Er soll Herr Kratzer, aber vor allem Manesh zufrieden stellen. Im anschluss lesen sie noch ein bisschen Geschwurbel, das einfach nur ein Paar Zeilen füllen soll: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-          "size": "1em"
-        }
-      }
-    }
-  };
-  
+
+var carouselData = [
+  {
+      bild: 'bilder/welpe1.jpg',
+      überschrift: 'Überschrift 1',
+      text: 'Text 1'
+  },
+  {
+      bild: 'bilder/welpe1.jpg',
+      überschrift: 'Überschrift 2',
+      text: 'Text 2'
+  },
+  // Mehr Slides hinzufügen
+];
+
+
+var currentSlide = 0;
+
+// Function to update the slide
+function updateSlide() {
+    var slide = carouselData[currentSlide];
+    document.getElementById('bild').src = slide.bild;
+    document.getElementById('überschrift').innerText = slide.überschrift;
+    document.getElementById('text').innerText = slide.text;
+}
+
+// Function to go to the next slide
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % carouselData.length; // Wrap around to the first slide
+    updateSlide();
+}
+
+// Function to go to the previous slide
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + carouselData.length) % carouselData.length; // Wrap around to the last slide
+    updateSlide();
+}
+
+// Update the slide when the page loads
+window.onload = function() {
+    updateSlide();
+    // Add event listeners to the arrow buttons
+    document.querySelector('.carousel-control-next').addEventListener('click', nextSlide);
+    document.querySelector('.carousel-control-prev').addEventListener('click', prevSlide);
+};
