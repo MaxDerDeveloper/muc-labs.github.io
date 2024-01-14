bd170d7e0f126c27bb912d44e18dba2ac6e62ad7
Cewtwo <128930660+Cewtwo@users.noreply.github.com>
2024-01-14 22:31:57 +0100

    Update material.js

--- js/material.js
+++ js/material.js
@@ -3,14 +3,14 @@ var material = {
 		"FabLab Laborrundgang": {
 			"name": "FabLab Laborrundgang",
 			"text": "Stephanie Legeleux zeigt das FabLab. Das Video ist ein Workshop-Mitschnitt (via Zoom) am virtuellen Schülerlabortag 2021.",
-			"images": ["/bilder/partner/lela_logo.png"],
+			"images": ["/material/fablab_1.jpeg"],
 			"website": "https://youtu.be/THREczNaz8M"
 		},
 
 		"FabLab Impressionen": {
 			"name": "FabLab Impressionen",
 			"text": "Dieses Video zeigt viele Beispiele aus dem FabLab München.",
-			"images": ["/bilder/partner/mint-region.png"],
+			"images": ["/material/fablab_2.jpeg"],
 			"website": "https://www.youtube.com/watch?v=yxgcpD1w9uI"
 		},
 
