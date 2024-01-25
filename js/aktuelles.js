296f67b036f790e221aaa509842b1f49f4e88ada
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-25 14:24:12 +0100

    Aktuelles links

--- js/aktuelles.js
+++ js/aktuelles.js
@@ -14,13 +14,13 @@ var aktuelleInhalte = [
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: 'Deutsche Physikmeisterschaft GYPT 2023 - Vorbereitungstreffen',
-      text: 'Die deutsche Physik-Meisterschaft (<a href="https://www.gypt.org">GYPT</a>) ist jetzt auch in die "Qualitätskriterien für Schülerwettbewerbe" der Kultusministerkonferenz (KMK) aufgenommen. Sie finden sie <a href="https://www.kmk.org/fileadmin/veroeffentlichungen_beschluesse/2009/2009_09_17-Schuelerwettbewerbe.pdf">hier</a>. Bei der Präsenzmeisterschaft 2020 entstand ein <a href="https://www.youtube.com/watch?v=zdjoeK3sNnw">youtube-Video</a> über den Wettbewerb. Ein sehenswertes <a href="https://www.youtube.com/watch?v=38YRTvaDL-w">Video</a> gibt es auch über die "oberbayerische Meisterschaft" in Garching.<br>Am <font color="red">16. November 2022 (Buß- und Bettag)</font> veranstaltete der GYPT Standort Dachau zusammen mit MUC-Labs (speziell PhotonLab) ein GYPT-Vorbereitungstreffen in Garching (MPQ). <font color="red">Die oberbayerische Meisterschaft fand Ende Januar 2023 statt. Zwei Teams konnten sich für die Deutsche Meisterschaft in Bad Honnef qualifizieren.</font><br> Unser Flyer <a href="bilder/Aktuelles/GYPT_Flyer_MUC_1.pdf">Seite 1</a>/<a href="bilder/Aktuelles/GYPT_Flyer_MUC_2.pdf">Seite 2</a>.',
+      text: 'Die deutsche Physik-Meisterschaft (<a href="https://www.gypt.org" class="warn-link">GYPT</a>) ist jetzt auch in die "Qualitätskriterien für Schülerwettbewerbe" der Kultusministerkonferenz (KMK) aufgenommen. Sie finden sie <a href="https://www.kmk.org/fileadmin/veroeffentlichungen_beschluesse/2009/2009_09_17-Schuelerwettbewerbe.pdf" class="warn-link">hier</a>. Bei der Präsenzmeisterschaft 2020 entstand ein <a href="https://www.youtube.com/watch?v=zdjoeK3sNnw" class="warn-link">youtube-Video</a> über den Wettbewerb. Ein sehenswertes <a href="https://www.youtube.com/watch?v=38YRTvaDL-w" class="warn-link">Video</a> gibt es auch über die "oberbayerische Meisterschaft" in Garching.<br>Am <font color="red">16. November 2022 (Buß- und Bettag)</font> veranstaltete der GYPT Standort Dachau zusammen mit MUC-Labs (speziell PhotonLab) ein GYPT-Vorbereitungstreffen in Garching (MPQ). <font color="red">Die oberbayerische Meisterschaft fand Ende Januar 2023 statt. Zwei Teams konnten sich für die Deutsche Meisterschaft in Bad Honnef qualifizieren.</font><br> Unser Flyer <a href="bilder/Aktuelles/GYPT_Flyer_MUC_1.pdf">Seite 1</a>/<a href="bilder/Aktuelles/GYPT_Flyer_MUC_2.pdf">Seite 2</a>.',
       datum: '16.11.2022',
     },
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: 'Formel 1 in der Schule 2022/2023',
-      text: 'Bis zum 30. November 2022 konnte man sich zu den Meisterschaften der 17. Saison von Formel 1 in der Schule anmelden.<br><a href="https://www.f1inschools.de">Weitere Informationen.</a>.',
+      text: 'Bis zum 30. November 2022 konnte man sich zu den Meisterschaften der 17. Saison von Formel 1 in der Schule anmelden.<br><a href="https://www.f1inschools.de" class="warn-link">Weitere Informationen.</a>.',
       datum: '30.11.2022',
     },
     {
@@ -32,31 +32,31 @@ var aktuelleInhalte = [
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: 'März 2022: Ausschreibung für Schulen der Deutschen Stiftung Denkmalschutz',
-      text: 'Im März 2022 gab es im Rahmen des Schulprogramms <i>denkmal aktiv - Kulturerbe macht Schule</i> der Deutschen Stiftung Denkmalschutz eine neue Ausschreibung <u>für Schulen</u>. Es konnte eine finanzielle Unterstützung beantragt werden, um im Verlauf eines Schuljahres Umwelteinflüsse auf Denkmäler in der Umgebung zu untersuchen. An Gymnasien könnte das z.B. innerhalb eines P- oder W-Seminars geschehen. Unterstützung werden dabei Schülerlabore anbieten. Auch das MUC-Labs Netzwerk wird sich mit einigen Laboren beteiligen.<br>Die Projekte sollen im Schuljahr 2022/2023 stattfinden.<br>Informationen und Beispiele gibt es bei <a href="https://denkmal-aktiv.de">denkmal-aktiv</a>.',
+      text: 'Im März 2022 gab es im Rahmen des Schulprogramms <i>denkmal aktiv - Kulturerbe macht Schule</i> der Deutschen Stiftung Denkmalschutz eine neue Ausschreibung <u>für Schulen</u>. Es konnte eine finanzielle Unterstützung beantragt werden, um im Verlauf eines Schuljahres Umwelteinflüsse auf Denkmäler in der Umgebung zu untersuchen. An Gymnasien könnte das z.B. innerhalb eines P- oder W-Seminars geschehen. Unterstützung werden dabei Schülerlabore anbieten. Auch das MUC-Labs Netzwerk wird sich mit einigen Laboren beteiligen.<br>Die Projekte sollen im Schuljahr 2022/2023 stattfinden.<br>Informationen und Beispiele gibt es bei <a href="https://denkmal-aktiv.de" class="warn-link">denkmal-aktiv</a>.',
       datum: '01.03.2022',//Datum unklar
     },
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: '14. März 2022: LeLa-Preis geht ans PhotonLab',
-      text: 'Auf der online-Jahrestagung von LernortLabor - dem Bundesverband der Schülerlabore e.V. fand am 14. März die diesjährige Verleihung des LeLa-Preises statt. In der Rubrik "Experiment des Jahres" war das PhotonLab erfolgreich mit dem Projekt: Quanten-Zufallsgenerator. Dieser Kurs wurde von Linda Qerimi entwickelt.<br>Mehr zum LeLa-Preis: <a href="https://www.lernortlabor.de/LernortLabor/LeLa-Preis">hier</a><br><iframe width="560" height="315" src="https://www.youtube.com/embed/NnR82jL3XBo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>',
+      text: 'Auf der online-Jahrestagung von LernortLabor - dem Bundesverband der Schülerlabore e.V. fand am 14. März die diesjährige Verleihung des LeLa-Preises statt. In der Rubrik "Experiment des Jahres" war das PhotonLab erfolgreich mit dem Projekt: Quanten-Zufallsgenerator. Dieser Kurs wurde von Linda Qerimi entwickelt.<br>Mehr zum LeLa-Preis: <a href="https://www.lernortlabor.de/LernortLabor/LeLa-Preis" class="warn-link">hier</a><br><iframe width="560" height="315" src="https://www.youtube.com/embed/NnR82jL3XBo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>',
       datum: '14.03.2022',
     },
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: '16. Juli 2021: 10 Jahre PhotonLab',
-      text: 'Das PhotonLab in Garching wurde 10! Am 16. Juli 2021 fand eine Lehrerfortbildung statt und am 23. Juli 2021 waren alle Interessierten eingeladen.<br>Mehr dazu auf den <a href="https://www.mpq.mpg.de/6539952/10-jahre-photonlab?c=2337">Webseiten des MPQ</a>.',
+      text: 'Das PhotonLab in Garching wurde 10! Am 16. Juli 2021 fand eine Lehrerfortbildung statt und am 23. Juli 2021 waren alle Interessierten eingeladen.<br>Mehr dazu auf den <a href="https://www.mpq.mpg.de/6539952/10-jahre-photonlab?c=2337" class="warn-link">Webseiten des MPQ</a>.',
       datum: '16.07.2021',
     },
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: '12. Mai 2021: „gemeinsam.Brücken.bauen." - Aufholen nach Corona in Bayern',
-      text: 'Durch zusätzliche Maßnahmen sollen Schülerinnen und Schüler, bei denen Defizite aufgrund der Pandemie entstanden sind, unterstützt werden. Dabei geht es nicht nur um Lernförderung, sondern auch um die Förderung sozialer Kompetenz. Das Kultusministerium hat dazu ein <a href="https://www.km.bayern.de/allgemein/meldung/7293/foerderprogramm-bietet-vielfaeltige-moeglichkeiten-zur-unterstuetzung.html">Konzept</a> bekannt gegeben.<br>Die finanzielle Grundlage bilden 20 Mio. Euro an Landesmitteln und die angekündigten Bundesmittel aus dem "Aktionsprogramm Aufholen nach Corona" (für Bayern voraussichtlich gut 150 Mio. Euro).',
+      text: 'Durch zusätzliche Maßnahmen sollen Schülerinnen und Schüler, bei denen Defizite aufgrund der Pandemie entstanden sind, unterstützt werden. Dabei geht es nicht nur um Lernförderung, sondern auch um die Förderung sozialer Kompetenz. Das Kultusministerium hat dazu ein <a href="https://www.km.bayern.de/allgemein/meldung/7293/foerderprogramm-bietet-vielfaeltige-moeglichkeiten-zur-unterstuetzung.html" class="warn-link">Konzept</a> bekannt gegeben.<br>Die finanzielle Grundlage bilden 20 Mio. Euro an Landesmitteln und die angekündigten Bundesmittel aus dem "Aktionsprogramm Aufholen nach Corona" (für Bayern voraussichtlich gut 150 Mio. Euro).',
       datum: '12.05.2021',
     },
     {
       bild: 'bilder/Aktuelles/Musterbild.png',
       überschrift: '5. Mai 2021: Aktionsprogramm Aufholen nach Corona',
-      text: 'In der Beschreibung der Säule 3 des Aktionsprogramms werden explizit Schülerlabore und der Bundesverband LernortLabor genannt. Auch die MUC-Labs werden ein Programm entwickeln und sich um Mittel bewerben. <a href="https://www.bmbf.de/de/kinder-und-jugendliche-nach-der-corona-pandemie-staerken-14371.html">(Pressemitteilung des BMBF)</a>',
+      text: 'In der Beschreibung der Säule 3 des Aktionsprogramms werden explizit Schülerlabore und der Bundesverband LernortLabor genannt. Auch die MUC-Labs werden ein Programm entwickeln und sich um Mittel bewerben. <a href="https://www.bmbf.de/de/kinder-und-jugendliche-nach-der-corona-pandemie-staerken-14371.html" class="warn-link">(Pressemitteilung des BMBF)</a>',
       datum: '05.05.2021',
     },
     {
